42e19aecee980ba6b24db169ef2238231913ec9f
Dylan Burton <burtond@uw.edu>
2025-12-11 11:44:34 -0800

    Initial commit: force Cmd+Tab to unhide apps

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- Force Alt-Tab (Cmd+Tab) to always unhide apps on macOS
+-- https://github.com/burtond/force-alt-tab
+--
+-- Problem: On macOS, Cmd+Tab to a hidden app doesn't always bring it forward.
+-- You have to press Cmd+Option while releasing Tab, or click the dock icon.
+--
+-- Solution: This script watches for app activation and automatically unhides
+-- the app and raises all its windows.
+
+require("hs.ipc")
+
+local unhideTimer = nil
+
+local appWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+    if eventType == hs.application.watcher.activated then
+        -- Small delay to let the switch complete
+        if unhideTimer then unhideTimer:stop() end
+        unhideTimer = hs.timer.doAfter(0.05, function()
+            local app = hs.application.frontmostApplication()
+            if app then
+                app:unhide()
+                -- Raise all windows to bring them forward
+                local wins = app:allWindows()
+                for _, win in ipairs(wins) do
+                    win:raise()
+                end
+            end
+        end)
+    end
+end)
+appWatcher:start()
